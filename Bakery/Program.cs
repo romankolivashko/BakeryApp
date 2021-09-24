d08b1c9ac430891a5b4eff665554872b20fdb3ed
Roman Kolivashko <rkolivashko@gmail.com>
2021-09-24 08:33:26 -0700

    Project setup

---
+++ Bakery/Program.cs
@@ -0,0 +1,14 @@
+using System;
+using Bakery.Models;
+
+namespace Bakery.Models
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Bakery bakery = new Bakery();
+        bakery.RunSale();
+        }
+    }
+}
