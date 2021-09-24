7df14cd738704baa9dd473683c5c7e24a20be444
Roman Kolivashko <rkolivashko@gmail.com>
2021-09-24 11:17:13 -0700

    Added UI

--- Bakery/Models/Bakery.cs
+++ Bakery/Models/Bakery.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace Bakery
@@ -33,7 +34,37 @@ namespace Bakery
       return _instances;
     }
 
+    public static void ClearBreadList()
+    {
+      _instances.Clear();
+    }
 
+    public static void DisplayBread()
+    {
+      for (int i = 0; i < Bread._instances.Count; i++)
+      {
+        if (Bread._instances[i].Quantity > 0)
+        {
+          Console.Write(Bread._instances[i].Description + " * " + Bread._instances[i].Quantity + ", ");
+        }
+      }
+    }
+
+    public static int BreadCount()
+    {
+      int finalCount = 0;
+      for (int i = 0; i < Bread._instances.Count; i++)
+      {
+        finalCount += Bread._instances[i].Quantity;
+      } 
+      return finalCount;
+    }
+
+    public static int BreadPrice()
+    {
+      int finalPrice = (Bread.BreadCount() * 5) - ((Bread.BreadCount() / 3) * 5);
+      return finalPrice;
+    }
 
   }
     public class Pastry
--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -3,12 +3,25 @@ using Bakery.Models;
 
 namespace Bakery.Models
 {
-    // class Program
-    // {
-    //     static void Main(string[] args)
-    //     {
-    //     Bakery bakery = new Bakery();
-    //     bakery.RunSale();
-    //     }
-    // }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        Console.WriteLine("Welcome to Pierre's Bakery!!!");
+        Console.WriteLine("We have on Sale Today:");
+        Console.WriteLine("Buy 2, get 1 free (every 3rd loaf of bread is free.)");
+        Console.WriteLine("A single loaf costs $5, two loaves cost $10, and three loaves cost $10.");
+        Console.WriteLine("How many loaves of bread would you like to buy? Enter number:");
+        int quantB = Int32.Parse(Console.ReadLine());
+        System.Console.WriteLine("You got it! " + quantB + " loaves of bread are coming up.");
+        Console.WriteLine("Care for some pastry as well? If so, enter how many:");
+        int quantP = Int32.Parse(Console.ReadLine());
+        System.Console.WriteLine("You got it! " + quantP + " piece(s) of pastry are coming up.");
+
+        System.Console.WriteLine("-------------------------");
+        System.Console.WriteLine("Your total is: ");
+        // Bread bread = new Bread();
+        // bread.GetAllBread();
+        }
+    }
 }
